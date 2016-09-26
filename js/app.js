0b9a44521918af204a98c2656fb7c203245fa7ff
Victor Jordan <victorjordan95@gmail.com>
2016-09-26 16:40:37 -0300

    addin img at what we do section

--- js/app.js
+++ js/app.js
@@ -13,12 +13,9 @@ app.controller("colaboradoresCtrl", function($scope){
 app.controller("servicesCtrl", function($scope){
 	$scope.app = "O que fazemos";
 	$scope.services = [
-		{title: "Sites", description: "Produzimos sites sobre medida para seu empreendimento, cuidando desde de uma apurada analise do que seu negócio precisa em um site até redação dos textos presentes nele.", imagem: ""},
-		{title: "Social Media", description: "Sua empresa precisa estar no facebook, interagindo com seus clientes e dando a eles conteúdos relacionados, deixe sua página sob nossos cuidados e vamos gerar conteúdo para sua página sempre ter novidades.", imagem: ""},
-		{title: "Mídia Impressa", description: "Faça conosco seus cartões de visitas com nossa equipe de criativos para melhorar a apresentação da sua empresa, folhetos, calendários e mouse-pads customizados e inovadores.", imagem: ""},
-		{title: "Visual de seu Ponto", description: "Porque não customizar seu ponto de venda com quadros, enfeites customizados e seu logo nos principais materiais, tudo alinhado com o que pretende apresentar ao cliente? Fazemos isto e muito mais para você!", imagem: ""},
-		{title: "Identidade Visual", description: "Toda empresa precisa de um simbolo e este simbolo precisa ser muito bem pensado, sobre este serviço temos desde uma equipe criativa para inovar focado no objetivo do logo até o preparo de uma versão final com manual de identidade visual. Animamos seu logo para que ele tenha movimento na sua página do facebook ou site.", imagem: ""},
-		{title: "StartUps", description: "Graças a nossa estrutura organizacional, atendemos tanto nossos clientes externos quanto produzimos startups, baseado em nossa experiência no mercado e ideias inovadoras produzimos apps e soluções fantásticas!", imagem: "" }
+		{title: "Sites", description: "Produzimos sites sobre medida para seu empreendimento, cuidando desde de uma apurada analise do que seu negócio precisa em um site até redação dos textos presentes nele.", imagem: "site.png"},
+		{title: "Identidade Visual", description: "Toda empresa precisa de um simbolo e este simbolo precisa ser muito bem pensado, sobre este serviço temos desde uma equipe criativa para inovar focado no objetivo do logo até o preparo de uma versão final com manual de identidade visual. Faça conosco seus cartões de visitas com nossa equipe de criativos para melhorar a apresentação da sua empresa, folhetos, calendários e mouse-pads customizados e inovadores.", imagem: "identidade.png"},
+		{title: "Social Media", description: "Sua empresa precisa estar no facebook, interagindo com seus clientes e dando a eles conteúdos relacionados, deixe sua página sob nossos cuidados e vamos gerar conteúdo para sua página sempre ter novidades.", imagem: "social-media.png"}
 	];
 });
 
