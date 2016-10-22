f77f8814b3dbe3c79fdc6b459fc0991fe5ed1e64
Victor Jordan <victorjordan95@gmail.com>
2016-10-22 19:08:24 -0200

    adding some jobs

--- js/js.js
+++ js/js.js
@@ -33,3 +33,12 @@ $(document).ready(function () {
 });
 
 
+$(document).ready(function() {
+$(".btn-pref .btn").click(function () {
+    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+    // $(".tab").addClass("active"); // instead of this do the below 
+    $(this).removeClass("btn-default").addClass("btn-primary");   
+});
+});
+
+
