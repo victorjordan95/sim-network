bc688c2203b3323d8b5e10c717c254d8d7f90cdc
Victor Jordan <victorjordan95@gmail.com>
2016-11-06 18:37:05 -0200

    no message

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@ app.controller("colaboradoresCtrl", function($scope){
 app.controller("servicesCtrl", function($scope){
 	$scope.app = "O que fazemos";
 	$scope.services = [
-		{title: "Sites", description: "Produzimos sites sobre medida para seu empreendimento, cuidando desde de uma apurada analise do que seu negócio precisa em um site até redação dos textos presentes nele.", imagem: "site.png"},
-		{title: "Identidade Visual", description: "Toda empresa precisa de um simbolo e este simbolo precisa ser muito bem pensado, sobre este serviço temos desde uma equipe criativa para inovar focado no objetivo do logo até o preparo de uma versão final com manual de identidade visual. Faça conosco seus cartões de visitas com nossa equipe de criativos para melhorar a apresentação da sua empresa, folhetos, calendários e mouse-pads customizados e inovadores.", imagem: "identidade.png"},
+		{title: "Sites", description: "Desenvolvidos sob medida para seu empreeendimento, levando em conta o que seu negócio precisa em um site com opção de redação dos textos presentes nele.", imagem: "site.png"},
+		{title: "Identidade Visual", description: "Toda empresa deve possuir um simbolo, deixe os detalhes e particularidades deste simbolo com nossa equipe criativa, com um logo inovador, cartão de visita chamativo e serviços gráficos totalmente customizados.", imagem: "identidade.png"},
 		{title: "Social Media", description: "Sua empresa precisa estar no facebook, interagindo com seus clientes e dando a eles conteúdos relacionados, deixe sua página sob nossos cuidados e vamos gerar conteúdo para sua página sempre ter novidades.", imagem: "social-media.png"}
 	];
 });
