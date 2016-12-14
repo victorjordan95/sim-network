3d04c66dd516f1c619e8294975dbef7c4288e6b6
Victor Jordan <victorjordan95@gmail.com>
2016-12-13 22:42:29 -0200

    angular with json

--- js/app.js
+++ js/app.js
@@ -1,22 +1,35 @@
-var app = angular.module("colaboradores", []);
+var app = angular.module("app", ["ngRoute"]);
 
-app.controller("colaboradoresCtrl", function($scope){
-	$scope.app = "Colaboradores";
-	$scope.colaboradores = [
-		{nome: "byCasseb", funcao:"CEO", link: "http://www.bycasseb.hol.es/", descricao: "By Casseb é a empresa que administra a iniciativa Sim Network utilizando seu potencial organizacional e criativo, cuidando das suas ações inovadoras.", imagem: 'bycasseb.jpg' },
-		{nome: "Victor Jordan",  funcao:"Front-end/UX Designer",link: "http://victorjordan.surge.sh", descricao: "Nosso front-end, com experiência com UX, amante de novas tecnologias e Star Wars, está sempre procurando aprender coisas novas  ", imagem: 'victor-jordan.jpg' },
-		{nome: "Pedro", funcao:"função", link: "", descricao: "breve texto sobre o colaborador", imagem: 'Logo Sim Network 2.1.png' },
-		{nome: "Pedro", funcao:"função", link: "", descricao: "breve texto sobre o colaborador", imagem: 'Logo Sim Network 2.1.png' }
-	];
+app.controller("mainCtrl", function($scope,$http,$routeParams){
+    $http.get("/json/colaboradores.json").then(function (response) {
+        $scope.colaboradores = response.data;
+    });
+    $http.get("/json/services.json").then(function(response){
+       $scope.services = response.data; 
+    });
 });
 
-app.controller("servicesCtrl", function($scope){
-	$scope.app = "O que fazemos";
-	$scope.services = [
-		{title: "Sites", description: "Desenvolvidos sob medida para seu empreeendimento, levando em conta o que seu negócio precisa em um site com opção de redação dos textos presentes nele.", imagem: "site.png"},
-		{title: "Identidade Visual", description: "Toda empresa deve possuir um simbolo, deixe os detalhes e particularidades deste simbolo com nossa equipe criativa, com um logo inovador, cartão de visita chamativo e serviços gráficos totalmente customizados.", imagem: "identidade.png"},
-		{title: "Social Media", description: "Sua empresa precisa estar no facebook, interagindo com seus clientes e dando a eles conteúdos relacionados, deixe sua página sob nossos cuidados e vamos gerar conteúdo para sua página sempre ter novidades.", imagem: "social-media.png"}
-	];
+app.controller('cvController', function ($scope, $http, $routeParams) {
+    $scope.name = "teste";
+    $http.get('/json/colaboradores/' + $routeParams.id + ".json").then(function (response) {
+        $scope.colaboradores = response.data;
+    });
+});
+
+app.config(function($routeProvider) {
+	$routeProvider
+    .when("/", {
+        templateUrl : "templates/home.html",
+        controller: 'mainCtrl'
+    })
+    .when("/cv/:id", {
+        templateUrl : "templates/cv.html",
+        controller : 'cvController'
+    })
+    .when("/cadastro", {
+        templateUrl : "templates/cadastro.htm",
+        controller: 'mainController'
+    })
 });
 
 
