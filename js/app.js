20f811d50ce3d34f6789fde23250646c5dd64937
Victor Jordan <victorjordan95@gmail.com>
2017-01-17 21:11:56 -0200

    Adding a new field about our team.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,88 @@
 var app = angular.module("app", ["ngRoute"]);
+app.service('anchorSmoothScroll', function () {
+    this.scrollTo = function (eID) {
+        // This scrolling function 
+        // is from http://www.itnewb.com/tutorial/Creating-the-Smooth-Scroll-Effect-with-JavaScript
+        var startY = currentYPosition();
+        var stopY = elmYPosition(eID);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY);
+            return;
+        }
+        var speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for (var i = startY; i < stopY; i += step) {
+                setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+                leapY += step;
+                if (leapY > stopY) leapY = stopY;
+                timer++;
+            }
+            return;
+        }
+        for (var i = startY; i > stopY; i -= step) {
+            setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+            leapY -= step;
+            if (leapY < stopY) leapY = stopY;
+            timer++;
+        }
 
-app.controller("mainCtrl", function($scope,$http,$routeParams){
+        function currentYPosition() {
+            // Firefox, Chrome, Opera, Safari
+            if (self.pageYOffset) return self.pageYOffset;
+            // Internet Explorer 6 - standards mode
+            if (document.documentElement && document.documentElement.scrollTop) return document.documentElement.scrollTop;
+            // Internet Explorer 6, 7 and 8
+            if (document.body.scrollTop) return document.body.scrollTop;
+            return 0;
+        }
+
+        function elmYPosition(eID) {
+            var elm = document.getElementById(eID);
+            var y = elm.offsetTop;
+            var node = elm;
+            while (node.offsetParent && node.offsetParent != document.body) {
+                node = node.offsetParent;
+                y += node.offsetTop;
+            }
+            return y;
+        }
+    };
+});
+app.controller("mainCtrl", function ($scope, $http, $routeParams, $location, $anchorScroll, anchorSmoothScroll) {
     $http.get("/json/colaboradores.json").then(function (response) {
         $scope.colaboradores = response.data;
     });
-    $http.get("/json/services.json").then(function(response){
-       $scope.services = response.data; 
+    $http.get("/json/services.json").then(function (response) {
+        $scope.services = response.data;
     });
+    $scope.gotoElement = function (eID) {
+        // set the location.hash to the id of
+        // the element you wish to scroll to.
+        $location.hash('bottom');
+        // call $anchorScroll()
+        anchorSmoothScroll.scrollTo(eID);
+    };
 });
-
 app.controller('cvController', function ($scope, $http, $routeParams) {
     $scope.name = "teste";
     $http.get('/json/colaboradores/' + $routeParams.id + ".json").then(function (response) {
         $scope.colaboradores = response.data;
     });
 });
-
-app.config(function($routeProvider) {
-	$routeProvider
-    .when("/", {
-        templateUrl : "templates/home.html",
-        controller: 'mainCtrl'
-    })
-    .when("/cv/:id", {
-        templateUrl : "templates/cv.html",
-        controller : 'cvController'
-    })
-    .when("/cadastro", {
-        templateUrl : "templates/cadastro.htm",
-        controller: 'mainController'
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "templates/home.html"
+        , controller: 'mainCtrl'
+    }).when("/cv/:id", {
+        templateUrl: "templates/cv.html"
+        , controller: 'cvController'
+    }).when("/cadastro", {
+        templateUrl: "templates/cadastro.htm"
+        , controller: 'mainController'
     })
 });
-
-
--- js/js.js
+++ js/js.js
@@ -1,44 +1,16 @@
-$(window).on("scroll", function() {
-    if($(window).scrollTop() > 50) {
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
         $(".header").addClass("active");
-    } else {
+    }
+    else {
         //remove the background property so it comes transparent again (defined in your css)
-       $(".header").removeClass("active");
+        $(".header").removeClass("active");
     }
 });
-
-
 $(document).ready(function () {
-  function filterPath(string) {
-    return string
-    .replace(/^\//, '')
-    .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
-    .replace(/\/$/, '');
-          }
-    $('.slide-menu').each(function () {
-      if (filterPath(location.pathname) == filterPath(this.pathname) && location.hostname == this.hostname && this.hash.replace(/#/, '')) {
-            var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) + ']');
-            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-              if ($target) {
-                var targetOffset = $target.offset().top;
-                  $(this).click(function () {
-                    if($(window).width() > 200){
-                      $('html, body').animate({scrollTop: targetOffset}, 800);
-                        return false;
-                          }
-                    });
-              }
-      }
-  });
+    $(".btn-pref .btn").click(function () {
+        $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+        // $(".tab").addClass("active"); // instead of this do the below 
+        $(this).removeClass("btn-default").addClass("btn-primary");
+    });
 });
-
-
-$(document).ready(function() {
-$(".btn-pref .btn").click(function () {
-    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-    // $(".tab").addClass("active"); // instead of this do the below 
-    $(this).removeClass("btn-default").addClass("btn-primary");   
-});
-});
-
-
