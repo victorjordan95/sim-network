a42cb37d1799e0d4ac5576f9dfc810d81db23dee
Victor Jordan <victorjordan95@gmail.com>
2017-01-18 15:01:08 -0200

    alteração na pagina dos times e ajustes no header

---
+++ js/controllers.js
@@ -0,0 +1,21 @@
+app.controller("mainCtrl", function ($scope, $http, $routeParams, $location, $anchorScroll, anchorSmoothScroll) {
+	$http.get("/json/colaboradores.json").then(function (response) {
+		$scope.colaboradores = response.data;
+	});
+	$http.get("/json/services.json").then(function (response) {
+		$scope.services = response.data;
+	});
+	$scope.gotoElement = function (eID) {
+		// set the location.hash to the id of
+		// the element you wish to scroll to.
+		$location.hash('bottom');
+		// call $anchorScroll()
+		anchorSmoothScroll.scrollTo(eID);
+	};
+});
+app.controller('cvController', function ($scope, $http, $routeParams) {
+	$scope.name = "teste";
+	$http.get('/json/colaboradores/' + $routeParams.id + ".json").then(function (response) {
+		$scope.colaboradores = response.data;
+	});
+});
---
+++ js/routes.js
@@ -0,0 +1,12 @@
+app.config(function ($routeProvider) {
+	$routeProvider.when("/", {
+		templateUrl: "templates/home.html"
+		, controller: 'mainCtrl'
+	}).when("/cv/:id", {
+		templateUrl: "templates/cv.html"
+		, controller: 'cvController'
+	}).when("/cadastro", {
+		templateUrl: "templates/cadastro.htm"
+		, controller: 'mainController'
+	})
+});
