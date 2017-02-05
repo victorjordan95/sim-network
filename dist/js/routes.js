d41df20c65bc934bdb66d88675cf990fc8d0af0a
Victor Jordan <victorjordan95@gmail.com>
2017-02-05 19:08:43 -0200

    optimization with gulp

---
+++ dist/js/routes.js
@@ -0,0 +1,15 @@
+app.config(function ($routeProvider) {
+	$routeProvider.when("/", {
+		templateUrl: "templates/home.html",
+        controller: 'mainCtrl'
+	}).when("/cv/:id", {
+		templateUrl: "templates/cv.html"
+		, controller: 'cvController'
+	}).when("/cadastro", {
+		templateUrl: "templates/cadastro.htm"
+		, controller: 'mainController'
+	}).when("/termos", {
+		templateUrl: "templates/termos.html"
+		, controller: 'mainController'
+	})
+});
